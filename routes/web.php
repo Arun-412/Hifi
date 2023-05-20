a84cc973cc0dbced028acb08727092288d5f40cc
Arun-412 <arunmozhi52892@gmail.com>
2023-05-20 19:08:23 +0530

    home page design

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hifi');
+    return view('hifimoney');
 });
 
 Route::get('/admin',  ['as'=>'admin.login','uses'=>'UserController@admin_login']);
